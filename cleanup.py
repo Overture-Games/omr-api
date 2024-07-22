7e379697af526250c4745f7c839d1dfc0fbfa1fd
Harry Guan <guanhuai2014@gmail.com>
2024-07-22 13:12:42 -0500

    update cleanup.py
    made path not hardcoded

--- cleanup.py
+++ cleanup.py
@@ -13,8 +13,12 @@ def remove_files_and_folders(directory):
         elif os.path.isdir(file_path):
             shutil.rmtree(file_path)
 
-output_directory = '/Users/hguan/Documents/GitHub/omr-api/output'
-uploads_directory = '/Users/hguan/Documents/GitHub/omr-api/uploads'
+# Get the directory of the current script
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the paths for output and uploads directories
+output_directory = os.path.join(current_directory, 'output')
+uploads_directory = os.path.join(current_directory, 'uploads')
 
 remove_files_and_folders(output_directory)
 remove_files_and_folders(uploads_directory)
