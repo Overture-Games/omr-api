fbe54692aeed65c0277962038409d0fe5234e4db
Harry Guan <guanhuai2014@gmail.com>
2024-07-23 17:29:30 -0500

    added nodemailer, basic mail config

--- public/script.js
+++ public/script.js
@@ -31,6 +31,32 @@ document.addEventListener('DOMContentLoaded', () => {
         handleFiles(files);
     }
 
+    // Get the modal
+    var modal = document.getElementById("contactUsModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("contactUsBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
     // Handle selected files
     function handleFiles(files) {
         const supportedFormats = ['image/jpeg', 'image/jpg', 'image/png', 'image/heic', 'image/svg+xml', 'application/pdf'];
